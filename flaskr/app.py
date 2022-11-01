b860664e00a60d29816c5f0fd2dc893d58e51251
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-10-31 21:27:05 -0500

    test converter

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,6 @@
 from flaskr import create_app
 from .modelos import db, Task
+from datetime import datetime
 # from flask_mail import Mail
 
 import subprocess
@@ -9,14 +10,12 @@ app = create_app('default')
 
 @app.cli.command()
 def converter():
-    # print('Hello world')
     db.init_app(app)
     # mail.init_app(app)
 
     tareas = db.session.query(Task).filter_by(state='uploaded')
     # if len(tareas) != 0:
     for tarea in tareas:                
-        # mypath = tarea.path
         idUser = tarea.id_usuario
         mypath = "../../audios/" + str(idUser)
         # print("el tipo de variable de mypath es {} y su valor es {}".format(type(mypath),mypath))
@@ -24,14 +23,15 @@ def converter():
         newformat = tarea.newformat
         try:
             # print("Hola tarea con id {}, el path es {} para el archivo {} y con el nuevo formato {} \n".format(tarea.id,mypath,filename,newformat))
-            
-            print("vamos a mirar si permite procesar el cambio de audio")                 
+            # print("vamos a mirar si permite procesar el cambio de audio")                 
 
             lista = "ftransc -f " + str(newformat) + " " + str(filename) + " --force-root -w"
             cmd = ['ftransc', '-f', str(newformat), str(filename), '--force-root','-w']
             # print(lista)
+            tarea.fecha_inicio = datetime.utcnow()
             subprocess.call(cmd,cwd=mypath)                    
             print("formato actualizado")
+            tarea.fecha_final = datetime.utcnow()
             tarea.state="procesed"
             db.session.add(tarea)
             db.session.commit()
