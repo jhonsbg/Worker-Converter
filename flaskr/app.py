ca78e6259cda0f3675a7fe6c0d7061392197ce26
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-10-31 20:52:03 -0500

    test path folder audios

--- flaskr/app.py
+++ flaskr/app.py
@@ -16,23 +16,26 @@ def converter():
     tareas = db.session.query(Task).filter_by(state='uploaded')
     # if len(tareas) != 0:
     for tarea in tareas:                
-        mypath = tarea.path
-#         print("el tipo de variable de mypath es {} y su valor es {}".format(type(mypath),mypath))
-        filename = tarea.filename
-        newformat = tarea.newformat
-        print(mypath + '' + filename + '' + newformat)
-#         try:
-#             print("Hola tarea con id {}, el path es {} para el archivo {} y con el nuevo formato {} \n".format(tarea.id,mypath,filename,newformat))
+        # mypath = tarea.path
+        idUser = tarea.iduser
+        mypath = "../../audios/" + tarea.id_usuario
+        print (mypath)
+        # print("el tipo de variable de mypath es {} y su valor es {}".format(type(mypath),mypath))
+        # filename = tarea.filename
+        # newformat = tarea.newformat
+        # # print(mypath + '' + filename + '' + newformat)
+        # try:
+        #     print("Hola tarea con id {}, el path es {} para el archivo {} y con el nuevo formato {} \n".format(tarea.id,mypath,filename,newformat))
             
-#             print("vamos a mirar si permite procesar el cambio de audio")                 
+        #     print("vamos a mirar si permite procesar el cambio de audio")                 
 
-#             lista = "ftransc -f " + str(newformat) + " " + str(filename) + " --force-root -w"
-#             cmd = ['ftransc', '-f', str(newformat), str(filename), '--force-root','-w']
-#             print(lista)
-#             subprocess.call(cmd,cwd=mypath)                    
-#             print("formato actualizado")
-#             tarea.state="procesed"
-#             db.session.add(tarea)
-#             db.session.commit()
-#         except:
-#             print("Error con la tarea que tiene id {}".format(tarea.id))
+        #     lista = "ftransc -f " + str(newformat) + " " + str(filename) + " --force-root -w"
+        #     cmd = ['ftransc', '-f', str(newformat), str(filename), '--force-root','-w']
+        #     print(lista)
+        #     subprocess.call(cmd,cwd=mypath)                    
+        #     print("formato actualizado")
+        #     tarea.state="procesed"
+        #     db.session.add(tarea)
+        #     db.session.commit()
+        # except:
+        #     print("Error con la tarea que tiene id {}".format(tarea.id))
