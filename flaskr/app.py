7bcd30be2b92a062e8a94cb6d4f7b2868c421402
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-11-21 22:57:58 -0500

    Update pub/sub configuration

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,80 +2,109 @@ from flaskr import create_app
 from flask import request
 from .modelos import db, Task
 from datetime import datetime
-# from flask_mail import Mail
 
-# Import Google Client Libraries
-from google.cloud import storage
-
+from concurrent.futures import TimeoutError
 import subprocess
 
+# Google Cloud Services
+from google.cloud import storage
+from google.cloud import pubsub_v1
+
 app = create_app('default')
-# mail = Mail()
 
 @app.cli.command()
 def converter():
     db.init_app(app)
-    # mail.init_app(app)
 
-    tareas = db.session.query(Task).filter_by(state='uploaded')
-    # if len(tareas) != 0:
-    for tarea in tareas:                
-        idUser = tarea.id_usuario
-        mypath = str(idUser) + "/"
-        # print("el tipo de variable de mypath es {} y su valor es {}".format(type(mypath),mypath))
-        filename = tarea.filename
-        newformat = tarea.newformat
+    timeout = 5.0 
+
+    # Subscriber Google Pub/Sub configuration
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = 'projects/cloud-andes-conversion-tool/subscriptions/Prueba-Audio-sub'
+
+    def callback(message):
+        print(f'Received message: {message}')
+        print(f'data: {message.data}')
+
+        if message.attributes:
+            print("Attributes:")
+            for key in message.attributes:
+                value = message.attributes.get(key)
+                print(f"{key}: {value}")
+
+            audioConverter(message.attributes["id"], message.attributes["filename"], message.attributes["newformat"])
+
+        message.ack()
+
+    with subscriber:                                                # wrap subscriber in a 'with' block to automatically call close() when done
         try:
-            # print("Hola tarea con id {}, el path es {} para el archivo {} y con el nuevo formato {} \n".format(tarea.id,mypath,filename,newformat))
-            # print("vamos a mirar si permite procesar el cambio de audio")                 
-            
-            # print("path donde se actualiza el archivo: " + mypath)                 
+            # streaming_pull_future.result(timeout=timeout)
+            streaming_pull_future.result()                          # going without a timeout will wait & block indefinitely
+        except TimeoutError:
+            streaming_pull_future.cancel()                          # trigger the shutdown
+            streaming_pull_future.result()   
 
-            # lista = "ftransc -f " + str(newformat) + " " + str(filename) + " --force-root -w"
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    print(f'Listening for messages on {subscription_path}')
 
-            # Dowload file from GCP Bucket
-            bucket_name = "audio_storage_cloud"
-            source_blob_name = filename
-            destination_file_name = filename
-            
-            storage_client = storage.Client()
-            bucket = storage_client.bucket(bucket_name)
-            blob = bucket.blob(source_blob_name)
-            blob.download_to_filename(destination_file_name)
+    # Database Query
+    #tareas = db.session.query(Task).filter_by(state='uploaded')
 
-            print(
-                "Downloaded storage object {} from bucket {} to local file {}.".format(
-                    source_blob_name, bucket_name, destination_file_name
+    def audioConverter(idUser, fileName, newFormat):
+            # Declare variables
+            idUser = idUser
+            filename = fileName
+            newformat = newFormat
+
+            mypath = str(idUser) + "/"
+            completePath = mypath + filename
+            name_file = filename[:-3]
+            new_name_file = name_file + newformat
+            newPathFile = mypath + new_name_file
+
+
+            try:            
+                # Dowload file from GCP Bucket
+                bucket_name = "audio_storage_cloud"
+                source_blob_name = completePath
+                destination_file_name = filename
+                
+                storage_client = storage.Client()
+                bucket = storage_client.bucket(bucket_name)
+                blob = bucket.blob(source_blob_name)
+                blob.download_to_filename(destination_file_name)
+
+                print(
+                    "Downloaded storage object {} from bucket {} to local file {}.".format(
+                        source_blob_name, bucket_name, destination_file_name
+                    )
                 )
-            )
+                
+                # Audio convert
+                cmd = ['ftransc', '-f', str(newformat), str(filename), '--force-root','-w']
+                # fecha_inicio = datetime.utcnow()
+                subprocess.call(cmd,cwd=mypath)                    
+                print("Formato actualizado")
 
-            cmd = ['ftransc', '-f', str(newformat), str(filename), '--force-root','-w']
-            # print(lista)
-            tarea.fecha_inicio = datetime.utcnow()
-            subprocess.call(cmd,cwd=mypath)                    
-            print("formato actualizado")
+                # Save the file in Bucket of GCP
+                bucket_name = "audio_storage_cloud"
+                destination_blob_name = newPathFile
+                contents = new_name_file
 
-            # Save the file in Bucket of GCP
-            bucket_name = "audio_storage_cloud"
-            destination_blob_name = mypath + filename
-            name_file = tarea.filename[:-3]
-            new_name_file = name_file + tarea.newformat
-            contents = new_name_file
+                storage_client = storage.Client()
+                bucket = storage_client.bucket(bucket_name)
+                blob = bucket.blob(destination_blob_name)
 
-            storage_client = storage.Client()
-            bucket = storage_client.bucket(bucket_name)
-            blob = bucket.blob(destination_blob_name)
+                blob.upload_from_file(request.files["file"], content_type='audio/mpeg')
 
-            blob.upload_from_file(request.files["file"], content_type='audio/mpeg')
+                print(
+                    f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+                )
 
-            print(
-                f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
-            )
-
-            # Update task information in DB
-            tarea.fecha_final = datetime.utcnow()
-            tarea.state="procesed"
-            db.session.add(tarea)
-            db.session.commit()
-        except:
-            print("Error con la tarea que tiene id {}".format(tarea.id))
+                # Update task information in DB
+                #tarea.fecha_final = datetime.utcnow()
+                #tarea.state="procesed"
+                #db.session.add(tarea)
+                #db.session.commit()
+            except:
+                print("Error con la tarea que tiene id")
